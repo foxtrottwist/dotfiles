e4609569f0f8f4b49d971c94aa50ea4f49edbc1b
Law Horne <hello@foxtrottwist.com>
2026-01-31 12:32:49 -0500

    fix: consolidate treesitter-textobjects into treesitter config

--- nvim/.config/nvim/lua/foxtrottwist/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/foxtrottwist/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     build = ":TSUpdate",
     dependencies = {
       "windwp/nvim-ts-autotag",
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
       -- import nvim-treesitter plugin safely
@@ -15,7 +16,8 @@ return {
       end
 
       -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
+      treesitter.setup({
+        -- enable syntax highlighting
         highlight = {
           enable = true,
         },
@@ -47,8 +49,41 @@ return {
         },
         -- auto install above language parsers
         auto_install = true,
+        -- textobjects configuration
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment region" },
+              ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment region" },
+              ["a:"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/field region" },
+              ["i:"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/field region" },
+              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional region" },
+              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional region" },
+              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop region" },
+              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop region" },
+              ["ab"] = { query = "@block.outer", desc = "Select outer part of a block region" },
+              ["ib"] = { query = "@block.inner", desc = "Select inner part of a block region" },
+              ["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+              ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            },
+            include_surrounding_whitespace = true,
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>on"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>op"] = "@parameter.inner",
+            },
+          },
+        },
       })
-      
+
       -- setup ts_context_commentstring separately with error handling
       local ts_ok, _ = pcall(require, "ts_context_commentstring")
       if ts_ok then
